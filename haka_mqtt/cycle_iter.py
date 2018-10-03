ab1434b724bcbcdfe53c66aa7df93516a0754348
Keegan Callin <kc@kcallin.net>
2018-10-02 21:00:00 -0600

    Committed cycle_iter.py.
    Signed-off-by: Keegan Callin <kc@kcallin.net>

--- haka_mqtt/cycle_iter.py
+++ haka_mqtt/cycle_iter.py
@@ -1,18 +1,28 @@
 class IntegralCycleIter(object):
-    def __init__(self, start, end):
-        """
+    """
 
-        Parameters
-        ----------
-        start: int
-        end: int
-        """
+    Parameters
+    ----------
+    start: int
+    end: int
+    """
+
+    def __init__(self, start, end):
         assert start < end
+        assert isinstance(start, int)
+        assert isinstance(end, int)
+
         self.start = start
         self.end = end
         self.__next = start
 
     def next(self):
+        """Returns the next iterator in the sequence.
+
+        Returns
+        -------
+        int
+        """
         n = self.__next
         if self.__next + 1 == self.end:
             self.__next = self.start

e001570a392c0a43beaad71b7a903b5a64d0cde0
Keegan Callin <kc@kcallin.net>
2018-10-17 21:08:17 -0600

    Removed unused imports from examples/main.py.

--- examples/main.py
+++ examples/main.py
@@ -9,7 +9,7 @@ from time import time
 from haka_mqtt.dns_async import AsyncFutureDnsResolver
 from haka_mqtt.socket_factory import SslSocketFactory
 from mqtt_codec.packet import MqttTopic, MqttControlPacketType
-from haka_mqtt.reactor import ReactorProperties, Reactor, ReactorState, INACTIVE_STATES
+from haka_mqtt.reactor import ReactorProperties, Reactor
 from haka_mqtt.clock import SystemClock
 from haka_mqtt.scheduler import Scheduler
 

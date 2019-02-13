a465a447bd78e269046de6f1a7e025ea1a24c4aa
Keegan Callin <kc@kcallin.net>
2019-02-12 23:21:31 -0700

    Updated documentation versions in conf.py.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2018 - 2019, Keegan Callin'
 author = u'Keegan Callin'
 
 # The short X.Y version
-version = u'0.3.4'
+version = u'0.3.5'
 # The full version, including alpha/beta/rc tags
-release = u'0.3.4'
+release = u'0.3.5'
 
 
 # -- General configuration ---------------------------------------------------
